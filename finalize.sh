ffe4ac8273d77d30a2fa1fe4b3637792285fb78f
Braden Stefanuk <bstefanuk@gmail.com>
2022-08-16 11:50:42 -0700

    Updates from mini-nuc install

--- finalize.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/zsh
-# /usr/bin/git --git-dir=/home/braden/.cfg/ --work-tree=/home/braden checkout 2>&1 | egrep '\s+\.' | awk {'print '} | xargs -I{} mv {} .config-backup/{}
-/usr/bin/git --git-dir=/home/braden/.cfg/ --work-tree=/home/braden config --local status.showUntrackedFiles no 
-/usr/bin/git --git-dir=/home/braden/.cfg/ --work-tree=/home/braden status 
-/usr/bin/git --git-dir=/home/braden/.cfg/ --work-tree=/home/braden restore ../.config/terminator/config ../.zshrc 
--- setup.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-sudo apt -y install git ansible
-ssh-keygen -t ed25519 -f id_ed25519
-
